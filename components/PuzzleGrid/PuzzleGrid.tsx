44a042276fdeed0f600fbe2c483ae5eba15b36bb
Juliann Stainbrook <hyperjoule@gmail.com>
2023-04-26 23:16:11 -0400

    slight refactor

--- components/PuzzleGrid/PuzzleGrid.tsx
+++ components/PuzzleGrid/PuzzleGrid.tsx
@@ -1,82 +1,90 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { View, Image, TouchableOpacity } from "react-native";
 import { getScreenDimensions } from "../../helpers/screenHelpers";
 import PuzzlePiece from "../PuzzlePiece/PuzzlePiece";
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface PuzzleGridProps {}
-
-const PuzzleGrid: React.FC<PuzzleGridProps> = () => {
+const PuzzleGrid = () => {
   const { width } = getScreenDimensions();
   const imageSize = width < 1024 ? Math.floor(width * 0.75) : 512;
-
-  const [imageURL, setImageURL] = useState("");
-  const [isShuffled, setIsShuffled] = useState(false);
   const numRows = 3;
   const numCols = 3;
   const pieceSize = imageSize / numRows;
 
+  const [imageURL, setImageURL] = useState("");
+  const [isShuffled, setIsShuffled] = useState(false);
+  const [piecePositions, setPiecePositions] = useState<number[]>([]);
+
+  const shuffleArray = useCallback((array: number[]) => {
+    return array.sort(() => Math.random() - 0.5);
+  }, []);
+
+  const fetchImage = useCallback(async () => {
+    const response = await fetch(
+      `https://picsum.photos/${imageSize}/${imageSize}`
+    );
+    setImageURL(response.url);
+    setPiecePositions(shuffleArray([...Array(numRows * numCols).keys()]));
+    setIsShuffled(true);
+  }, [imageSize, shuffleArray]);
+
   useEffect(() => {
-    const fetchImage = async () => {
-      const response = await fetch(
-        `https://picsum.photos/${imageSize}/${imageSize}`
-      );
-      setImageURL(response.url);
-    };
-
     fetchImage();
-  }, [imageSize]);
-  const shuffledIndices = [...Array(numRows * numCols).keys()].sort(
-    () => Math.random() - 0.5
-  );
+  }, [fetchImage]);
 
-  const [piecePositions, setPiecePositions] = useState(shuffledIndices);
-
-  const updatePiecePosition = (fromRow: number, fromCol: number) => {
-    const fromIndex = fromRow * numCols + fromCol;
-    const toIndex = piecePositions.findIndex((index) => index === fromIndex);
-
-    const newPiecePositions = [...piecePositions];
-    [newPiecePositions[fromIndex], newPiecePositions[toIndex]] = [
-      newPiecePositions[toIndex],
-      newPiecePositions[fromIndex]
-    ];
-
-    setPiecePositions(newPiecePositions);
+  const checkIfPuzzleIsComplete = () => {
+    return piecePositions.every((value, index) => value === index);
   };
 
-  const puzzlePieces = [];
-  if (imageURL && isShuffled) {
-    for (let row = 0; row < numRows; row++) {
-      for (let col = 0; col < numCols; col++) {
-        const index = row * numCols + col;
-        const shuffledIndex = piecePositions[index];
-        const toRow = Math.floor(shuffledIndex / numCols);
-        const toCol = shuffledIndex % numCols;
-        puzzlePieces.push(
-          <PuzzlePiece
-            key={`${row}-${col}`}
-            imageURL={imageURL}
-            pieceSize={pieceSize}
-            fromRow={row}
-            fromCol={col}
-            toRow={toRow}
-            toCol={toCol}
-            numRows={numRows}
-            numCols={numCols}
-            onPieceTap={updatePiecePosition}
-          />
-        );
+  const updatePiecePosition = useCallback(
+    (fromRow: number, fromCol: number) => {
+      const fromIndex = fromRow * numCols + fromCol;
+      const toIndex = piecePositions.findIndex((index) => index === fromIndex);
+
+      const newPiecePositions = [...piecePositions];
+      [newPiecePositions[fromIndex], newPiecePositions[toIndex]] = [
+        newPiecePositions[toIndex],
+        newPiecePositions[fromIndex]
+      ];
+
+      setPiecePositions(newPiecePositions);
+
+      if (checkIfPuzzleIsComplete()) {
+        fetchImage();
       }
-    }
-  }
+    },
+    [numCols, piecePositions, fetchImage]
+  );
+
+  const puzzlePieces =
+    imageURL &&
+    isShuffled &&
+    piecePositions.map((shuffledIndex, index) => {
+      const fromRow = Math.floor(index / numCols);
+      const fromCol = index % numCols;
+      const toRow = Math.floor(shuffledIndex / numCols);
+      const toCol = shuffledIndex % numCols;
+      return (
+        <PuzzlePiece
+          key={`${fromRow}-${fromCol}`}
+          imageURL={imageURL}
+          pieceSize={pieceSize}
+          fromRow={fromRow}
+          fromCol={fromCol}
+          toRow={toRow}
+          toCol={toCol}
+          numRows={numRows}
+          numCols={numCols}
+          onPieceTap={updatePiecePosition}
+        />
+      );
+    });
 
   return (
     <View style={{ width: imageSize, height: imageSize }}>
-      {imageURL && isShuffled ? (
+      {puzzlePieces && puzzlePieces.length > 0 ? (
         puzzlePieces
       ) : (
-        <TouchableOpacity onPress={() => setIsShuffled(true)}>
+        <TouchableOpacity onPress={fetchImage}>
           {imageURL && (
             <Image
               source={{ uri: imageURL }}
@@ -88,5 +96,4 @@ const PuzzleGrid: React.FC<PuzzleGridProps> = () => {
     </View>
   );
 };
-
 export default PuzzleGrid;
